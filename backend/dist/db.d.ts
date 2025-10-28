ee48b99bc3e69eb70b7dae5b9eda2671d3f228b0
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-10-28 19:22:56 +0000

    Delete backend/dist directory

--- backend/dist/db.d.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { Pool } from 'pg';
-import * as schema from "./schema.js";
-export declare const pool: Pool;
-export declare const db: import("drizzle-orm/node-postgres").NodePgDatabase<typeof schema> & {
-    $client: Pool;
-};
-//# sourceMappingURL=db.d.ts.map
